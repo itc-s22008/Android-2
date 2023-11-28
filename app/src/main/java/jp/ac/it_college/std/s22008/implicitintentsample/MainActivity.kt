5bf09bcfdbbf481e61fe5478533160007a4e5de2
s22008 <s22008@std.it-college.ac.jp>
2023-11-28 11:32:09 +0900

    first commit

---
+++ app/src/main/java/jp/ac/it_college/std/s22008/implicitintentsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.it_college.std.s22008.implicitintentsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ac.it_college.std.s22008.media3servicesample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
